e4b9a4adcc22a8b1a26c84ac66a9e1f34d9cbfa5
Kholmansky Maxim <maximkholmansky@gmail.com>
2017-10-17 21:46:26 +0700

    Basic controls / buttons and actions

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Maxim Kholmansky on 17/10/2017.
+//  Copyright © 2017 Maxim Kholmansky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert(){
+        print ("Hello World!")
+    }
+
+
+}
+
