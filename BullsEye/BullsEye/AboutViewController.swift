c12d79e3a55183d4c2d7e69d544e69a8efd27f84
Kholmansky Maxim <maximkholmansky@gmail.com>
2017-10-18 23:35:41 +0700

    Coding practice / adding-extra-screens

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Maxim Kholmansky on 18/10/2017.
+//  Copyright © 2017 Maxim Kholmansky. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
