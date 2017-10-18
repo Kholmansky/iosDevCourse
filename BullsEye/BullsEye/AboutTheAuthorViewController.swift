bc5cdd67ac4f794b9f7e1656c0627a6927465166
Kholmansky Maxim <maximkholmansky@gmail.com>
2017-10-18 23:58:49 +0700

    Coding practice / adding-extra-screens-2

---
+++ BullsEye/BullsEye/AboutTheAuthorViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AboutTheAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Maxim Kholmansky on 18/10/2017.
+//  Copyright © 2017 Maxim Kholmansky. All rights reserved.
+//
+
+import UIKit
+
+class AboutTheAuthorViewController: UIViewController {
+
+    @IBAction func closeTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
