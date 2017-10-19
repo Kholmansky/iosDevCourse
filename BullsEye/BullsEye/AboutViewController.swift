315ff803073b6ba7b56bba6d67133bab310a64ae
Kholmansky Maxim <maximkholmansky@gmail.com>
2017-10-19 20:07:59 +0700

    Styling the app / web-views

--- BullsEye/BullsEye/AboutViewController.swift
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class AboutViewController: UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    
+    
     @IBAction func close () {
         dismiss(animated: true, completion: nil)
     
@@ -17,6 +20,14 @@ class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html"){
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
