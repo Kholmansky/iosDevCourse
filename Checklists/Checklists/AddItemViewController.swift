f3578599d2da30a060b51da3a8483dbd1a869440
Kholmansky Maxim <maximkholmansky@gmail.com>
2017-11-15 22:54:24 +0700

    Checklists / control events

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
+
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
     }
@@ -36,5 +39,17 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
     }
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)
+        let newText = oldText.replacingCharacters(in: stringRange!, with: string)
+        if newText.isEmpty{
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
     }
 }
